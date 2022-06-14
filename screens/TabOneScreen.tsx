b0ccdc40a5644e29c3ada6c0e89e279eb63d6a5e
Ingeun Kim <ingeun92@naver.com>
2022-06-14 11:29:58 +0900

    add Camera module

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,178 @@
-import { StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+import { Camera, CameraType } from "expo-camera";
+import { useState, useEffect } from "react";
+
+export default function TabOneScreen() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(CameraType.back);
+
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState<any>(null);
+  const [metadata, setMetadata] = useState("");
+
+  let camera: Camera;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const CameraPictureOptions = {
+    exif: true,
+  };
+
+  const takePicture = async () => {
+    if (!camera) return;
+    const photo = await camera.takePictureAsync(CameraPictureOptions);
+
+    setPreviewVisible(true);
+    setCapturedImage(photo);
+    setMetadata(JSON.stringify(photo.exif));
+  };
+
+  const retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+
+    async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+  };
+
+  const CameraPreview = ({ photo, metadata, retakePicture }: any) => {
+    return (
+      <View style={styles.cameraPreview}>
+        <ImageBackground
+          source={{ uri: photo && photo.uri }}
+          style={{
+            flex: 1,
+          }}
+        >
+          <Text style={styles.metaInfo}>{metadata}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.buttonRetake}
+              onPress={retakePicture}
+            >
+              <Text style={styles.text}>Re-Take</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <>
+      {previewVisible && capturedImage ? (
+        <CameraPreview
+          photo={capturedImage}
+          metadata={metadata}
+          retakePicture={retakePicture}
+        ></CameraPreview>
+      ) : (
+        <Camera
+          type={type}
+          style={styles.container}
+          ref={(ref: Camera) => {
+            camera = ref;
+          }}
+        >
+          <View style={styles.cameraView}>
+            <View style={styles.buttonContainer}>
+              <View style={styles.buttonRow}>
+                <TouchableOpacity
+                  onPress={takePicture}
+                  style={styles.buttonTake}
+                />
+              </View>
+            </View>
+          </View>
+        </Camera>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  cameraView: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "transparent",
+    flexDirection: "row",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  cameraPreview: {
+    backgroundColor: "transparent",
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    flex: 1,
+    width: "100%",
+    padding: 20,
+    justifyContent: "space-between",
+    backgroundColor: "transparent",
+  },
+  buttonRow: {
+    alignSelf: "center",
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  button: {
+    flex: 0.5,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  buttonRetake: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  buttonTake: {
+    width: 70,
+    height: 70,
+    bottom: 0,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+  metaInfo: {
+    position: "absolute",
+    backgroundColor: "rgba(59, 59, 59, 0.6)",
+    width: "92%",
+    height: "50%",
+    color: "white",
+    fontSize: 15,
+    textAlign: "left",
+    paddingLeft: 10,
+    paddingTop: 5,
+    marginLeft: "4%",
+    marginTop: "10%",
   },
 });
